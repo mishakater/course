f7ea89d368abcdfdba43190ff99bff33458191f9
Misha <mihanya2011@gmail.com>
2019-09-27 13:37:55 +0300

    Completed task

--- script1.js
+++ script1.js
@@ -3,11 +3,16 @@
 let money = prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
+let answ1_1 = prompt("Введите обязательную статью расходов в этом месяце");
+let answ1_2 = prompt("Во сколько обойдется?");
+
+
 let appData = {
     budget : money,
     timeData : time,
     expenses : {
-
+        answ1_1:answ1_2
     }
-
 };
+
+alert("Бюджет на день" + money/30);
